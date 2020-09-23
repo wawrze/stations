b4c53a4ef2ff79b0b5b73238b63179204ad44936
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2020-09-23 15:10:41 +0200

    polish translation, fixes for older api levels, minSdkVersion lowered to 18

--- app/src/main/java/com/wawra/stations/App.kt
+++ app/src/main/java/com/wawra/stations/App.kt
@@ -1,6 +1,8 @@
 package com.wawra.stations
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.multidex.MultiDex
 import com.wawra.stations.di.components.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
@@ -10,6 +12,11 @@ open class App : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         return DaggerApplicationComponent.builder().application(this).build()
+    }
+
+    override fun attachBaseContext(context: Context?) {
+        super.attachBaseContext(context)
+        MultiDex.install(this)
     }
 
 }
