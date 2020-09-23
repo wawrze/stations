9fe7e3724cdafefbbe5c772c948a4e6a6c4d01d0
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2020-09-23 08:06:26 +0200

    MainViewModel get distance method + unit tests

--- app/src/main/java/com/wawra/stations/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/wawra/stations/presentation/main/MainViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.wawra.stations.base.BaseViewModel
 import com.wawra.stations.database.entities.Station
+import com.wawra.stations.logic.calculations.DistanceCalculator
 import com.wawra.stations.logic.repositories.StationRepository
 import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
+import io.reactivex.schedulers.Schedulers.computation
 import io.reactivex.schedulers.Schedulers.io
 import javax.inject.Inject
 
@@ -14,11 +16,17 @@ class MainViewModel @Inject constructor(var stationRepository: StationRepository
 
     private val mStations = MutableLiveData<List<Station>>()
     private val mGetStationsResult = MutableLiveData<Boolean>()
+    private val mDistance = MutableLiveData<Double>()
+    private val mGetDistanceResult = MutableLiveData<Boolean>()
 
     val stations: LiveData<List<Station>>
         get() = mStations
     val getStationsResult: LiveData<Boolean>
         get() = mGetStationsResult
+    val distance: LiveData<Double>
+        get() = mDistance
+    val getDistanceResult: LiveData<Boolean>
+        get() = mGetDistanceResult
 
     fun getMatchingStations(text: String) {
         stationRepository.getStationsByKeyword(text)
@@ -31,6 +39,26 @@ class MainViewModel @Inject constructor(var stationRepository: StationRepository
                 },
                 {
                     mGetStationsResult.postValue(false)
+                }
+            )
+            .addToDisposables()
+    }
+
+    fun getDistance(station1: Station, station2: Station) {
+        DistanceCalculator.calculateDistanceInKm(
+            station1.latitude,
+            station1.longitude,
+            station2.latitude,
+            station2.longitude
+        ).subscribeOn(computation())
+            .observeOn(mainThread())
+            .subscribe(
+                {
+                    mDistance.postValue(it)
+                    mGetDistanceResult.postValue(true)
+                },
+                {
+                    mGetDistanceResult.postValue(false)
                 }
             )
             .addToDisposables()
--- app/src/test/java/com/wawra/stations/presentation/main/MainViewModelTestSuite.kt
+++ app/src/test/java/com/wawra/stations/presentation/main/MainViewModelTestSuite.kt
@@ -2,10 +2,9 @@ package com.wawra.stations.presentation.main
 
 import com.wawra.stations.BaseTestSuite
 import com.wawra.stations.database.entities.Station
+import com.wawra.stations.logic.calculations.DistanceCalculator
 import com.wawra.stations.logic.exceptions.DataOutOfDateException
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import io.reactivex.Single
 import org.junit.After
 import org.junit.Assert.*
@@ -87,6 +86,96 @@ class MainViewModelTestSuite : BaseTestSuite() {
         val resultStations = objectUnderTest.stations.value
         assertTrue(actionResult == false)
         assertNull(resultStations)
+    }
+
+    @Test
+    fun shouldGetDistance() {
+        // given
+        val station1 = Station(
+            1L,
+            "station1",
+            "station1slug",
+            0.1234,
+            5.6789,
+            21,
+            31L,
+            "city1",
+            "region1",
+            "country1",
+            "name1"
+        )
+        val station2 = Station(
+            2L,
+            "station2",
+            "station2slug",
+            9.876,
+            5.4321,
+            22,
+            32L,
+            "city2",
+            "region2",
+            "country2",
+            "name2"
+        )
+        mockkObject(DistanceCalculator)
+        // when
+        every {
+            DistanceCalculator.calculateDistanceInKm(0.1234, 5.6789, 9.876, 5.4321)
+        } returns Single.just(123.45)
+        objectUnderTest.getDistance(station1, station2)
+        // then
+        verify { DistanceCalculator.calculateDistanceInKm(0.1234, 5.6789, 9.876, 5.4321) }
+        val actionResult = objectUnderTest.getDistanceResult.value
+        val resultDistance = objectUnderTest.distance.value
+        assertTrue(actionResult == true)
+        assertEquals(123.45, resultDistance ?: 0.0, 0.0)
+        // clean up
+        unmockkAll()
+    }
+
+    @Test
+    fun shouldNotGetDistance() {
+        // given
+        val station1 = Station(
+            1L,
+            "station1",
+            "station1slug",
+            0.1234,
+            5.6789,
+            21,
+            31L,
+            "city1",
+            "region1",
+            "country1",
+            "name1"
+        )
+        val station2 = Station(
+            2L,
+            "station2",
+            "station2slug",
+            9.876,
+            5.4321,
+            22,
+            32L,
+            "city2",
+            "region2",
+            "country2",
+            "name2"
+        )
+        mockkObject(DistanceCalculator)
+        // when
+        every {
+            DistanceCalculator.calculateDistanceInKm(0.1234, 5.6789, 9.876, 5.4321)
+        } returns Single.error(Exception())
+        objectUnderTest.getDistance(station1, station2)
+        // then
+        verify { DistanceCalculator.calculateDistanceInKm(0.1234, 5.6789, 9.876, 5.4321) }
+        val actionResult = objectUnderTest.getDistanceResult.value
+        val resultDistance = objectUnderTest.distance.value
+        assertTrue(actionResult == false)
+        assertNull(resultDistance)
+        // clean up
+        unmockkAll()
     }
 
 }
